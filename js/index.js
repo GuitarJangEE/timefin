35b2506cee22d76e7f5210f44b7dcdf58aa839f7
GuitarJangEE <144888249+GuitarJangEE@users.noreply.github.com>
2023-09-24 05:21:43 +0900

    타이머추가요일추가

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const 타이틀바꿈 = (document.title = "시계디이");
 const 일번시계 = document.querySelector(".time");
 const 이번시계 = document.getElementById("time");
-
+const 시계아님 = document.getElementById("stop");
 const 일번시계버튼 = {
   색바꿈: () => {
     if (일번시계.style.color === "green") {
@@ -53,13 +53,45 @@ const 진짜시계 = () => {
   const 분 = 실시간.getMinutes().toString().padStart(2, "0");
   const 초 = 실시간.getSeconds().toString().padStart(2, "0");
   const 요일 = ["일", "월", "화", "수", "목", "금", "토"];
-  const 년월일 = `${년}년${월}월${일}일${요일[요일숫자]}요일`;
-  const 시분초 = `${시}시${분}분${초}초`;
-  const 위쪽 = document.querySelector(".time");
-  const 아래쪽 = document.querySelector("#time");
+  const 년월일 = `${년}년 ${월}월 ${일}일 ${요일[요일숫자]}요일`;
+  const 시분초 = `${시}시 ${분}분 ${초}초`;
+  const 위쪽 = 일번시계;
+  const 아래쪽 = 이번시계;
   위쪽.innerText = 년월일;
   아래쪽.innerText = 시분초;
 };
+const 시작시간 = new Date();
+const 타이머 = () => {
+  const 현재시간 = new Date();
+  const 스탑워치 = new Date(현재시간 - 시작시간);
+  const 시간 = 스탑워치.getHours();
+  const 시간고침 = [시간 - 9].toString().padStart(2, "0");
+  const 분 = 스탑워치.getMinutes().toString().padStart(2, "0");
+  const 초 = 스탑워치.getSeconds().toString().padStart(2, "0");
+  const 위쪽 = 시계아님;
+  위쪽.innerText = `　　지난시간
+  ${시간고침}시간 ${분}분 ${초}초`;
+  //   Object.keys(스탑워치정리).forEach((a) => {
+  //     console.log(`${a}: ${스탑워치정리[a]}`);
+  //   });
+};
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+};
+
+// // 여러 속성을 호출하는 방법 1: for...in 반복문 사용
+// for (const key in person) {
+//   console.log(`${key}: ${person[key]}`);
+// }
+
+// // 여러 속성을 호출하는 방법 2: Object.keys와 forEach 사용
+// Object.keys(person).forEach((key) => {
+//   console.log(`${key}: ${person[key]}`);
+// });
+setInterval(타이머, 1000);
 const 초기화 = () => {
   일번시계.style.color = "black";
   일번시계.style.backgroundColor = "white";
